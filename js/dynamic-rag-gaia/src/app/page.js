4e9ab0304e7eec7aa274c9f21bf3ac02cff56e81
Harish Kotra <harishkotra@gmail.com>
2025-05-16 14:36:32 +0530

    adding dynamicrag demo

---
+++ js/dynamic-rag-gaia/src/app/page.js
@@ -0,0 +1,10 @@
+// src/app/page.js
+import DynamicRAG from '../components/DynamicRAG'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <DynamicRAG />
+    </main>
+  )
+}
